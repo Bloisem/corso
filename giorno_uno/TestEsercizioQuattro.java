59269c6c5aa1300f206048e9b46da73f246ee866
Bloisem <bloisemi@gmail.com>
2022-07-14 23:23:41 +0200

    Add: TestEsercizioQuattro

---
+++ giorno_uno/TestEsercizioQuattro.java
@@ -0,0 +1,24 @@
+package giorno_uno;
+
+import java.util.Scanner;
+
+public class TestEsercizioQuattro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// import classi
+		Scanner sc = new Scanner(System.in);
+		NumeriPrimi esercizioQuattro = new NumeriPrimi();
+		
+		System.out.println("Inserisci il numero: ");
+		int num = sc.nextInt();
+		esercizioQuattro.setNumeroInserito(num);
+		
+		String result = esercizioQuattro.divisoriPositivi();
+		System.out.println(result);
+
+	}
+
+}
