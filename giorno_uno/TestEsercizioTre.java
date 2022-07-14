3f30b1c9f3181ef82fdbc3ce9aa9c491e6639c25
Bloisem <bloisemi@gmail.com>
2022-07-14 22:18:32 +0200

    Add TestEsercizioTre

---
+++ giorno_uno/TestEsercizioTre.java
@@ -0,0 +1,25 @@
+package giorno_uno;
+
+import java.util.Scanner;
+
+public class TestEsercizioTre {
+
+	public static void main(String[] args) {
+		// import classi
+		Scanner sc = new Scanner(System.in);
+		NumeriPrimi esercizioTre = new NumeriPrimi();
+		
+
+		// input
+		System.out.println("Inserisci il numero: ");
+		int num = sc.nextInt();
+		
+		esercizioTre.setNumeroInserito(num);
+		
+		System.out.println(esercizioTre.numeriPrimi());
+
+		
+
+	}
+
+}
