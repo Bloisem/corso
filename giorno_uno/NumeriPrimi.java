8f099a2d7047e30ee524ee8c79950e5dca06e505
Bloisem <bloisemi@gmail.com>
2022-07-14 23:29:13 +0200

    Feat: NumeriPrimi

--- giorno_uno/NumeriPrimi.java
+++ giorno_uno/NumeriPrimi.java
@@ -3,6 +3,7 @@ package giorno_uno;
 public class NumeriPrimi {
 	private int numeroInserito;
 	
+	
 	public void setNumeroInserito(int a) {
 		if (a<1) {
 			System.out.println("Inserisci il numero maggiore di 0!");
@@ -26,4 +27,24 @@ public class NumeriPrimi {
 		}
 		return count==2?"Il numero è primo!":"Il numero non é primo! ";
 }
+	public String divisoriPositivi() {
+		StringBuilder temp = new StringBuilder();
+		StringBuilder temp1 = new StringBuilder();
+		int count = 0;
+		
+		for(int i = 2; i<numeroInserito; i++) {
+			if(numeroInserito%i==0) {
+				count++; 
+				temp.append(i).append(" ");
+			}
+		}
+		if(count ==0) {
+			temp1.append("Il numero non ha divisori positivi!");
+		}
+		else {
+			temp1.append("Il numero ha ").append(count).append(" divisori, ").append("e sono ").append(temp);
+			 
+		}
+		return temp1.toString();
+	}
 }
