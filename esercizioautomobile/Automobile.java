f702c2bd26238a6baa50c20cb83fbaf4ccc3cdd9
Bloisem <bloisemi@gmail.com>
2022-07-15 10:24:33 +0200

    Modificate: Automobile, AutomobileTest

--- esercizioautomobile/Automobile.java
+++ esercizioautomobile/Automobile.java
@@ -5,25 +5,44 @@ public class Automobile {
 	private String modello;
 	private int prezzo;
 
-public void setMarca(String m) {
-	marca = m;
-	
-}
-public String getMarca() {
-	return marca;
-}
-public void setModello(String n) {
-	modello = n;
-	
-}
-public String getModello() {
-	return modello;
-}
-public void setPrezzo(int p) {
-	prezzo = p;
-	
-}
-public int getPrezzo() {
-	return prezzo;
-}
+	public Automobile(String marca, String modello, int prezzo) {
+		this.marca = marca;
+		this.modello = modello;
+		this.prezzo = prezzo;
+	}
+
+	public Automobile() {
+
+	}
+
+	public void setMarca(String m) {
+		marca = m;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setModello(String n) {
+		modello = n;
+
+	}
+
+	public String getModello() {
+		return modello;
+	}
+
+	public void setPrezzo(int p) {
+		prezzo = p;
+
+	}
+
+	public int getPrezzo() {
+		return prezzo;
+	}
+
+	public String toString() {
+		String str = marca + " " + modello +": "+ "prezzo " + prezzo;
+		return str;
+	}
 }
--- esercizioautomobile/AutomobileTest.java
+++ esercizioautomobile/AutomobileTest.java
@@ -9,14 +9,21 @@ public class AutomobileTest {
 		fiesta.setModello("Fiesta");
 		fiesta.setPrezzo(15000);
 		
-		System.out.println(fiesta.getMarca() + " "+fiesta.getModello() + " prezzo: " + fiesta.getPrezzo());
+		//System.out.println(fiesta.getMarca() + " "+fiesta.getModello() + " prezzo: " + fiesta.getPrezzo());
 		
-		Automobile juke = new Automobile();
-		juke.setModello("Juke");
-		juke.setMarca("Nissan");
-		juke.setPrezzo(23000);
+		Automobile juke = new Automobile("Juke", "Nissan", 25000);
+		Automobile focus = new Automobile();
 		
-		System.out.println(juke.getMarca() + " "+juke.getModello() + " prezzo: " + juke.getPrezzo());
+		focus.setModello("Focus");
+		focus.setMarca("Ford");
+		focus.setPrezzo(18000);
+		
+		
+		
+		//System.out.println(juke.getMarca() + " "+juke.getModello() + " prezzo: " + juke.getPrezzo());
+		System.out.println(focus.toString());
+		System.out.println(juke.toString());
+		System.out.println(fiesta.toString());
 
 	}
 
