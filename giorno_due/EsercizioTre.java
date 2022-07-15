6edbfdc01f2cfb765615b23866dd43786eddc562
Bloisem <bloisemi@gmail.com>
2022-07-15 14:11:35 +0200

    Esercizi giorno_due

---
+++ giorno_due/EsercizioTre.java
@@ -0,0 +1,21 @@
+package giorno_due;
+
+public class EsercizioTre {
+
+	public static void main(String[] args) {
+		int[] array1 = {1,2,3,4,5,};
+		int[] array2 = {6,7,8,9,10};
+		
+		
+		for(int i = 0; i<array1.length; i++) {
+			for(int j = 0; j<array2.length; j++) {
+			System.out.print(array1[i]*array2[j]+" ");
+			}
+		System.out.println();
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
