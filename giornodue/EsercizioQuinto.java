55af534c239510ffb65c2f720195e4a5a47f6a95
Bloisem <bloisemi@gmail.com>
2022-07-18 01:00:33 +0200

    Change:giornodue

---
+++ giornodue/EsercizioQuinto.java
@@ -0,0 +1,38 @@
+package giornodue;
+
+import java.util.Scanner;
+
+public class EsercizioQuinto {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		
+		while (true) {
+			boolean a = true;
+			System.out.println("Inserisci una parola, exit per uscire");
+			String myStri = sc.nextLine();
+			if (myStri.equals("exit")) {
+				break;
+			}
+			else if (myStri.length() < 2) {
+				continue;
+			} 
+			else {
+				for (int i = 0; i < myStri.length()/2; i++) {
+					if (myStri.charAt(i) != myStri.charAt((myStri.length() - 1)-i)) {
+						a = false;
+					}
+				}
+				if (a == false) {
+					System.out.println("La parola non è palindroma");
+					
+				} 
+				else {
+					System.out.println("La parola è palindroma");
+					
+				}
+			}
+		}
+	}
+}
---
+++ giornodue/EsercizioUno.java
@@ -0,0 +1,35 @@
+package giornodue;
+
+import java.util.Scanner;
+
+public class EsercizioUno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inserisci il numero > 0!");
+		
+		
+		int numero = sc.nextInt();
+		
+		if (numero>0)
+		{System.out.println(numero*-1);}
+		else {
+			System.out.println("Inserisci il numero >0!");
+		}
+		
+	}
+		
+}	
+				
+				
+				
+			
+		
+		
+			
+	
+	
+
+
