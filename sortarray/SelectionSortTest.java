3166031300c35bfc552ef8ba580da1d6369bd23a
Bloisem <95679487+Bloisem@users.noreply.github.com>
2022-07-21 21:53:37 +0200

    Add: SortArrayTest

---
+++ sortarray/SelectionSortTest.java
@@ -0,0 +1,19 @@
+package selectionsort;
+
+public class SelectionSortTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//input
+		int []startArray = {100, 1,2,3,4,8,29,-96,-41,2,7,6,5,-100};
+		
+		//elaborazione
+		SelectionSort sortSelectionArray = new SelectionSort();
+		int [] resultArray =sortSelectionArray.selectionSort(startArray, false);
+		sortSelectionArray.printArray(resultArray);
+
+	}
+
+	
+
+}
